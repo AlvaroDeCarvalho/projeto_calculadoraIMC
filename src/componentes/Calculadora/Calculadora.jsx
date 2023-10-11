0ee9d2298d73eb91d0f347dc466b66260c1cb45a
AlvaroDeCarvalho <alvarodecarvalho20@gmail.com>
2023-10-10 22:15:19 -0300

    adicionando classificação

--- src/componentes/Calculadora/Calculadora.jsx
+++ src/componentes/Calculadora/Calculadora.jsx
@@ -1,13 +1,32 @@
-import styles from './Calc.module.css'
+import { useState, useEffect } from 'react';
+import styles from './Calc.module.css';
 
-const Calculadora = ({peso, altura}) => {
-    return(
-    <>
-    
-    <h3 className={styles.imc}>SEU IMC É DE: <b className={styles.resultado}>{isNaN(parseFloat(peso)/parseFloat(altura)) ? '' : parseFloat(peso)/parseFloat(altura)}</b></h3>
-    
-    </>
-    )
-}
+const Calculadora = ({ peso, altura }) => {
+    const imc = parseFloat(peso) / parseFloat(altura);
+    const [classificacao, setClassificacao] = useState('');
 
-export default Calculadora
+    useEffect(() => {
+        if (!isNaN(imc)) {
+            if (imc <= 18.5) {
+                setClassificacao('a baixo do peso');
+            } else if (imc >= 18.5 && imc <= 24.9) {
+                setClassificacao('eutrofia');
+            } else if (imc >= 25 && imc <= 29.9) {
+                setClassificacao('Sobrepeso');
+            } else {
+                setClassificacao('obeso');
+            }
+        }
+    }, [imc]);
+
+    return (
+        <>
+            <div className={styles.imc}>
+                <h3>SEU IMC É DE: <b className={styles.resultado}>{isNaN(imc) ? '' : imc}</b></h3>
+                <p className={styles.classificacaoImc}>{classificacao}</p>
+            </div>
+        </>
+    );
+};
+
+export default Calculadora;
